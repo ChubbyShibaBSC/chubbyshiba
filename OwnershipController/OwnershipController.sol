e4bd57d30b4ce9959138882d6f88f2ce11185028
ChubbyShibaBSC <support@chubbyshiba.com>
2021-05-15 11:42:56 -0700

    Add ownable and swapAndLiquify

--- OwnershipController/OwnershipController.sol
+++ OwnershipController/OwnershipController.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "AccessControl.sol";
+import "Ownable.sol";
 
 // Dummy contract for interface
 interface IChubbyShiba {
@@ -18,10 +19,14 @@ interface IChubbyShiba {
 	function setTaxFeePercent(uint256 taxFee) external;
 	
 	function setLiquidityFeePercent(uint256 liquidityFee) external;
+
+	function setSwapAndLiquifyEnabled(bool _enabled) external;
+
+	function renounceOwnership() external;
 }
 
 // Contract to hold ownership of ChubbyShiba
-contract OwnershipController is AccessControl {
+contract OwnershipController is AccessControl, Ownable {
 	bytes32 public constant EXECUTOR_ROLE = keccak256("EXECUTOR_ROLE");
 
 	/**
@@ -64,5 +69,13 @@ contract OwnershipController is AccessControl {
 	function setLiquidityFeePercent(address target, uint256 liquidityFee) public virtual onlyRole(EXECUTOR_ROLE) {
 		require(liquidityFee <= 10, "OwnershipController: liquidityFee must be less than or equal to 10");
 		IChubbyShiba(target).setLiquidityFeePercent(liquidityFee);
+	}
+
+	function setSwapAndLiquifyEnabled(address target, bool _enabled) public onlyRole(EXECUTOR_ROLE) {
+		IChubbyShiba(target, _enabled);
+	}
+
+	function renounceTargetOwnership(address target) public virtual onlyRole(EXECUTOR_ROLE) {
+		IChubbyShiba(target).renounceOwnership();
 	}
 }
